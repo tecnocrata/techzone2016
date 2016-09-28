2a568f7fd902f16b96306583778d93e1a03a5f51
Enrique <enrique@ortuno.net>
2016-09-28 15:10:40 -0400

    Sending working

---
+++ simple/sender.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+var q = 'hello';
+//amqp://abx-admin:abx01@host.com/vhost
+var amqp = require('amqplib/callback_api');
+//var open = require('amqplib').connect('amqp://localhost');
+var open = require('amqplib').connect('amqp://abx-admin:abx01@localhost');
+
+/*amqp.connect('amqp://abx-admin:abx01@localhost/', function(err, conn) {
+  console.log ('conn value '+conn);
+  conn.createChannel(function(err, ch) {
+    
+    var msg = 'Hello World!';
+
+    ch.assertQueue(q, {durable: false});
+    // Note: on Node 6 Buffer.from(msg) should be used
+    ch.sendToQueue(q, new Buffer(msg));
+    console.log(" [x] Sent %s", msg);
+  });
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});*/
+
+open.then(function(conn) {
+  return conn.createChannel();
+}).then(function(ch) {
+  return ch.assertQueue(q).then(function(ok) {
+    return ch.sendToQueue(q, new Buffer('something to do'));
+  });
+}).catch(console.warn);

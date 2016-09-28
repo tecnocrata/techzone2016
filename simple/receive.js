54dadb9eed5e271e5d92044adb0929f39daff6ad
Enrique <enrique@ortuno.net>
2016-09-28 18:46:31 -0400

    Modifying receiver to add promises

--- simple/receive.js
+++ simple/receive.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-
+//https://github.com/squaremo/amqp.node
+var q = 'hello';
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://abx-admin:abx01@localhost', function(err, conn) {
+/*amqp.connect('amqp://abx-admin:abx01@localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var q = 'hello';
 
     ch.assertQueue(q, {durable: true});
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
@@ -12,4 +12,18 @@ amqp.connect('amqp://abx-admin:abx01@localhost', function(err, conn) {
       console.log(" [x] Received %s", msg.content.toString());
     }, {noAck: true});
   });
-});
+});*/
+
+var open = require('amqplib').connect('amqp://abx-admin:abx01@localhost');
+open.then(function(conn) {
+  return conn.createChannel();
+}).then(function(ch) {
+  return ch.assertQueue(q).then(function(ok) {
+    return ch.consume(q, function(msg) {
+      if (msg !== null) {
+        console.log(msg.content.toString());
+        ch.ack(msg);
+      }
+    });
+  });
+}).catch(console.warn);

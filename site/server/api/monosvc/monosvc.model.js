bad84e1d38dc007000fb79dc8777e5f1e5e90734
osboxes.org <enrique@ortuno.net>
2016-10-11 03:31:31 +0100

    Adding all columns to table

--- site/server/api/monosvc/monosvc.model.js
+++ site/server/api/monosvc/monosvc.model.js
@@ -3,14 +3,11 @@
 import mongoose from 'mongoose';
 
 var MonosvcSchema = new mongoose.Schema({
-/*  name: String,
-  info: String,
-  active: Boolean,*/
     imageName: String, //uploaded
-  userNotified: Boolean, //notified
-  starts: Number, //badges
-  resized: Boolean, //resized
-  tweeted: Boolean //tweeted
+    userNotified: Boolean, //notified
+    stars: Number, //badges
+    resized: Boolean, //resized
+    tweeted: Boolean //tweeted
 });
 
 export default mongoose.model('Monosvc', MonosvcSchema);

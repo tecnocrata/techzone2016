bf32dd9dfc38304c3acb1029c115c107c4e76493
osboxes.org <enrique@ortuno.net>
2016-10-16 00:52:57 +0100

    Final corrections

--- site/client/app/app.js
+++ site/client/app/app.js
@@ -15,9 +15,23 @@ angular.module('techzoneApp', [
 
     $locationProvider.html5Mode(true);
     ngstompProvider
-                .url('http://localhost:15674/stomp') // previously ws://localhost:15674/ws or /ws or 
+                .url('ws://localhost:15674/ws') // previously _ or http://localhost:15674/stomp, doesn't work with /ws only
+                .credential('guest', 'guest')
+                .vhost('/')
+                //.debug(true);
+
+    //Working with sockjs but recomended for old browsers only
+    /*ngstompProvider
+                .url('http://localhost:15674/stomp') // doesn't work with ws://localhost:15674/ws or /ws
                 .credential('abx-admin', 'abx01')
                 .vhost('/')
                 .class(SockJS)
-                .debug(true);
+                .debug(true);*/
+              
+    //Working with web sockets with guest user, I have created the user not sure if work without it
+    /*ngstompProvider
+                .url('ws://localhost:15674/ws') // previously _ or http://localhost:15674/stomp, doesn't work with /ws only
+                .credential('guest', 'guest')
+                .vhost('/')
+                .debug(true);   */
   });
